772cf4e6188edf2a7f42b5d9786b87a8d77a2be6
Christian Rades <c.rades@shopware.com>
2023-02-23 13:35:39 +0100

    Add embedding selection

---
+++ web/embedding.py
@@ -0,0 +1,13 @@
+import os
+
+
+def get_embedding_fn():
+    if "OPENAI_API_KEY" in os.environ:
+        from langchain.embeddings.openai import OpenAIEmbeddings
+
+        return OpenAIEmbeddings()
+    else:
+        from langchain.embeddings import TensorflowHubEmbeddings
+
+        url = "https://tfhub.dev/google/universal-sentence-encoder-multilingual/3"
+        return TensorflowHubEmbeddings(model_url=url)
