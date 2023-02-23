196dba9380bf8455897a58f6e9af2a94fe9020f8
Christian Rades <c.rades@shopware.com>
2023-02-23 11:10:49 +0100

    Add post endpoint for queries

--- web/main.py
+++ web/main.py
@@ -1,12 +1,16 @@
 from typing import Union
 from fastapi import FastAPI
 
+from pydantic import BaseModel
+
+
 from .ingest import ingest
 from .query import query
 
 import logging
 
 app = FastAPI()
+
 
 @app.get("/")
 def read_root():
@@ -17,22 +21,43 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
+
 @app.post("/ingest")
 def post_ingest():
     return {"success": ingest()}
 
+
 @app.get("/query/{search}")
-def post_query(search: str):
+def get_query(search: str):
     results = query(search)
     mappedResults = []
 
     for result in results:
-        source = result[0].metadata['source']
+        source = result[0].metadata["source"]
         excerpt = result[0].page_content
         score = result[1]
         mappedResults.append({"source": source, "score": str(score)})
-    
+
     return {"results": mappedResults}
+
+
+class Query(BaseModel):
+    query: str
+
+
+@app.post("/query")
+def post_query(search: Query):
+    results = query(search.query)
+    mappedResults = []
+
+    for result in results:
+        source = result[0].metadata["source"]
+        excerpt = result[0].page_content
+        score = result[1]
+        mappedResults.append({"source": source, "score": str(score)})
+
+    return {"results": mappedResults}
+
 
 # https://ahmadrosid.com/blog/deploy-fastapi-flyio
 # https://fly.io/docs/languages-and-frameworks/python/
