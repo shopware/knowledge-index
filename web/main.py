25d806c62ec4b8633aaee1be2ec190e27b3752d9
Bojan Rajh <b.rajh@shopware.com>
2023-02-28 08:58:51 +0100

    Extract upload

--- web/main.py
+++ web/main.py
@@ -8,6 +8,7 @@ import os
 import glob
 import shutil
 
+from .upload import upload
 from .ingest import ingest, ingest_diff
 from .query import query, query_by_id
 from .config import data_dir
@@ -56,38 +57,7 @@ async def post_upload_input(
     if isinstance(collection, Union[str, None]):
         collection = CollectionParam(collection=collection)
 
-    input_zip = "input.zip"
-    output_dir = data_dir(collection.collection)
-
-    if os.path.exists(input_zip):
-        os.remove(input_zip)
-
-    length = 0
-    async with aiofiles.open(input_zip, "wb") as output:
-        while chunk := await content.read(1024):
-            length += len(chunk)
-            await output.write(chunk)
-
-    if os.path.exists(output_dir):
-        files = glob.glob(output_dir + "/*")
-        for f in files:
-            file_path = os.path.join(output_dir, f)
-            if os.path.isfile(file_path) or os.path.islink(file_path):
-                os.unlink(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(
-                    file_path,
-                )
-
-    with zipfile.ZipFile(input_zip, "r") as zip_ref:
-        zip_ref.extractall(output_dir)
-
-    # async do the diff + ingestion + backup
-
-    return {
-        "length": length,
-        "collection": collection.collection
-    }
+    return upload(content, collection.collection)
 
 
 @app.post("/ingest")
---
+++ web/upload.py
@@ -0,0 +1,40 @@
+
+import os
+import aiofiles
+import glob
+import shutil
+import zipfile
+
+def upload(content: UploadFile, collection):
+    input_zip = "input.zip"
+    output_dir = data_dir(collection)
+
+    if os.path.exists(input_zip):
+        os.remove(input_zip)
+
+    length = 0
+    async with aiofiles.open(input_zip, "wb") as output:
+        while chunk := await content.read(1024):
+            length += len(chunk)
+            await output.write(chunk)
+
+    if os.path.exists(output_dir):
+        files = glob.glob(output_dir + "/*")
+        for f in files:
+            file_path = os.path.join(output_dir, f)
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(
+                    file_path,
+                )
+
+    with zipfile.ZipFile(input_zip, "r") as zip_ref:
+        zip_ref.extractall(output_dir)
+
+    # async do the diff + ingestion + backup
+
+    return {
+        "length": length,
+        "collection": collection
+    }
