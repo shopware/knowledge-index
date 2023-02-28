152d1bb6b1b75a6325cdf2afdf7eaa2b74ce81a1
Dominic Klein <dominic_klein@yahoo.com>
2023-02-28 11:33:05 +0100

    docs: added endpoint descriptions

--- web/main.py
+++ web/main.py
@@ -46,13 +46,22 @@ app.add_middleware(
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    """Hello World"""
+    return { "Hello": "World" }
 
 
 @app.post("/upload-input")
 async def post_upload_input(
     content: UploadFile, collection: Union[CollectionParam, None, str] = CollectionParam()
 ):
+    """
+    Upload a zip file containing .md files to be ingested. A collection name can be provided to organize the documents in multiple indices.
+
+    | param | description |
+    | --- | --- |
+    | `content` | .zip file containing .md files |
+    | `collection` | Collection name |
+    """
     # workaround - https://fastapi.tiangolo.com/tutorial/request-forms-and-files/#define-file-and-form-parameters
     if isinstance(collection, Union[str, None]):
         collection = CollectionParam(collection=collection)
@@ -62,16 +71,40 @@ async def post_upload_input(
 
 @app.post("/ingest")
 def post_ingest(collection: CollectionParam = CollectionParam()):
+    """
+    Ingest a collection of documents. This will create a new index and create and store vector embeddings for each document.
+
+
+    | param | description |
+    | --- | --- |
+    | `collection` | Collection name |
+    """
     return {"success": ingest(collection.collection)}
 
 
 @app.post("/ingest-diff")
 def post_ingest(collection: CollectionParam = CollectionParam()):
+    """
+    Ingest unindexed documents of a collection. This will create and store vector embeddings for each uningested document.
+    If a document is already indexed, it will be skipped, if no document is indexed a new index will be created.
+
+    | param | description |
+    | --- | --- |
+    | `collection` | Collection name |
+    """
     return {"success": ingest_diff(collection.collection)}
 
 
 @app.post("/query")
 def post_query(data: PostQueryParams):
+    """
+    Query a collection based on a search query. This will return the 5 closest documents based on the vector embeddings.
+
+    | param | description |
+    | --- | --- |
+    | `search` | Search query |
+    | `collection` | Collection name |
+    """
     results = query(data.search, data.collection)
 
     return {"results": map_results(results)}
@@ -79,6 +112,17 @@ def post_query(data: PostQueryParams):
 
 @app.post("/neighbours")
 def post_query(data: PostNeighboursParams):
+    """
+    Obtain the closest neighbours for a given document id. This will return the 5 closest documents based on the vector embeddings.
+
+    An id is the relative file name of the .md file - for example: `src/docs/products/extensions/migration-assistant/concept/dataselection-and-dataset.md`
+
+
+    | param | description |
+    | --- | --- |
+    | `id` | Relative file name of a document |
+    | `collection` | Collection name |
+    """
     results = query_by_id(data.id, data.collection)
     results = [
         result for result in results if result[0].metadata["source"] != data.id
@@ -93,4 +137,5 @@ def post_query(data: PostNeighboursParams):
 # https://fly.io/docs/reference/secrets/#setting-secrets
 @app.get("/healthcheck")
 def healtcheck():
+    """Healthcheck endpoint"""
     return {"status": "ok"}
