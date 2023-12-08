4115cd8ff8062c76433ae701ab9d148aee2fbc4b
Bojan Rajh <b.rajh@shopware.com>
2023-12-08 10:43:22 +0100

    feat: import 5 sources

--- cli/support.py
+++ cli/support.py
@@ -2,35 +2,92 @@
 import typer
 import sys
 import csv
-import re
+
 from markdownify import markdownify as md
 from slugify import slugify
+from bs4 import BeautifulSoup
 
 app = typer.Typer()
 
 @app.command('demo')
-def demo():
+def demo(input: str):
     # python cli/support.py demo
 
     csv.field_size_limit(sys.maxsize)
 
+    files = {
+        'Docs_export.csv': {
+            'source': 'docs',
+            'id': 0,
+            'title': 1,
+            'description': 2,
+        },
+        'Jira_Next.csv': {
+            'source': 'jira-next',
+            'id': 2,
+            'title': 0,
+            'description': 37,
+        },
+        'uservoice_comments.csv': {
+            'source': 'uservoice-comments',
+            'id': 0,
+            'title': 3,
+            'description': 1,
+        },
+        'uservoice_notes.csv': {
+            'source': 'uservoice-notes',
+            'id': 0,
+            'title': 3,
+            'description': 1,
+        },
+        'uservoice_suggestions.csv': {
+            'source': 'uservoice-suggestions',
+            'id': 0,
+            'title': 1,
+            'description': 2,
+        },
+    }
+
     totalLength = 0
-    with open('./cli/exports/Docs_export.csv', mode ='r') as file:
+    with open('./cli/exports/' + input, mode ='r', newline='', encoding='utf-8') as file:
         csvFile = csv.reader(file)
+        i = 0
         for lines in csvFile:
-            if len(lines[2]) == 0:
+            i = i + 1
+
+            #if i <= 2:
+            #    print(lines)
+
+            # skip first row
+            if i == 1:
                 continue
 
-            markdown = '# ' + lines[1] + "\n\n" + md(re.sub("\n\n", "\n", lines[2]))
+            fileConfig = files[input]
+            endDir = "./cli/transformed/" + fileConfig["source"] + "/"
+
+            title = lines[fileConfig["title"]]
+            destination = lines[fileConfig["id"]] + "-" + slugify(lines[fileConfig["title"]])
+            #print(title)
+            
+            content = lines[fileConfig["description"]].replace('\n\n', '\n')
+            ##content = BeautifulSoup(content, "xml")
+            ##content = content.prettify()
+
+            #print(content)
+            
+            if len(content) == 0:
+                continue
+
+            markdown = '# ' + title + "\n\n" + md(content, heading_style='ATX').replace('\n\n', '\n')
             
             totalLength = totalLength + len(markdown)
 
-            f = open("./cli/transformed/docs/" + lines[0] + "-" + slugify(lines[1]) + ".md", "w")
+            f = open(endDir + destination + ".md", "w")
             f.write(markdown)
             f.close()
 
-            f = open("./cli/transformed/docs/" + lines[0] + "-" + slugify(lines[1]) + ".html", "w")
-            f.write(lines[2])
+            f = open(endDir + destination + ".html", "w")
+            f.write(content)
             f.close()
     
     print("Total length: " + str(totalLength))
